c69d950be254f49f110b6cc534beda77cdd3b6cd
Alexander03e <cfifeg1@gmail.com>
2024-01-16 01:25:47 +0300

    add main route

---
+++ src/pages/Main.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const MainPage = styled.div`
+  margin: 0 auto;
+  text-align:center;
+`
+
+export const Main = () => {
+
+
+  return (
+    <MainPage>
+      <h2>мой бренд</h2>
+    </MainPage>
+  )
+}
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,11 +3,13 @@ import { Catalog } from '../components/Catalog/Catalog'
 import { CardDetail } from '../components/Card/CardDetail'
 import { Arrive } from '../pages/Arrive'
 import { About } from '../pages/About'
+import { Main } from '../pages/Main'
 
 
 export const MainRoutes = () => {
   
   const basedPath = [
+    {path: '/', element: <Catalog />},
     {path: '/catalog', element: <Catalog />},
     {path: '/catalog/:itemid', element: <CardDetail />},
     {path: '/arrive', element: <Arrive />},
